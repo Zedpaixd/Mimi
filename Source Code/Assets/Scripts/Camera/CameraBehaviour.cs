8e6b7361cc5c4c113090a4e21795551ac5cf695b
Zedpaixd <planta636@gmail.com>
2022-09-29 22:45:37 +0300

    Camera behaviour

---
+++ Source Code/Assets/Scripts/Camera/CameraBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	public Transform target;
+	public float dampingTime = 0.25f;
+	public float PPU = 64f; //Pixels per unit
+
+	private Vector3 velocity;
+
+	private Vector3 proxyPosition;
+
+	private void LateUpdate() {
+
+		proxyPosition = Vector3.SmoothDamp(proxyPosition, target.position, ref velocity, dampingTime);
+
+
+		transform.position = new Vector3(
+
+		Mathf.Round(proxyPosition.x * PPU)/PPU,
+		Mathf.Round(proxyPosition.y * PPU)/PPU,
+		-10f
+
+		);
+	}
+
+
+}
---
+++ Source Code/Assets/Scripts/Camera/CameraTargetBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTargetBehaviour : MonoBehaviour {
+
+	public float distance = 4;
+
+	private PlayerMovement player;
+
+	void Start() {
+		player = PlayerMovement.instance;
+	}
+
+	void Update() {
+
+		if (player.GetDirection() != 0)
+		{
+		Vector3 localPos = new Vector3(
+			player.GetDirection() < 0 ? -1f : 1.6f,
+			0f,
+			0f
+		);
+		transform.localPosition = localPos * distance;
+		}
+
+	}
+
+}
