97196d388846362cd3d9efe34c40a7161cbba937
Vykthor36 <victor.laurin.pro@gmail.com>
2022-10-25 15:22:48 +0300

    First level pattern

---
+++ Source Code/Assets/Scripts/Globals.cs
@@ -0,0 +1,18 @@
+public static class Globals
+{
+    #region tags
+    public static string PLAYER_TAG = "Player";
+    public static string GROUND_TAG = "Ground";
+    public static string COLLECTIBLE_TAG = "collectible";
+    #endregion
+
+    #region input
+    public static string HORIZONTAL_AXIS = "Horizontal";
+    #endregion
+
+    #region magic numbers
+    public static int SLOW_ON_JUMP = 3;
+    public static int DELTA_SMOOTHENING = 5;
+
+    #endregion
+}
