baa0c24db9b70437f616c1a61adfce07231d1b6e
Armand Alexandru Balint <70021545+Zedpaixd@users.noreply.github.com>
2022-10-27 14:06:06 +0300

    Revert "Level design"

---
+++ Source Code/Assets/Scripts/Audio/SceneAudio.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneAudio : MonoBehaviour
+{
+    public AudioClip bgm;
+
+    public AudioClip GetBGM()
+    {
+        return bgm;
+    }
+}
