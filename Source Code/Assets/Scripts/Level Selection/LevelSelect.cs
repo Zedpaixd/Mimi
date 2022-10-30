d0c7acde8fff0e3ee4e182dfb69b2c7bd02c23a6
Armand Alexandru Balint <planta636@gmail.com>
2022-10-30 22:28:58 +0200

    Unsure of what this changes

--- Source Code/Assets/Scripts/Level Selection/LevelSelect.cs
+++ Source Code/Assets/Scripts/Level Selection/LevelSelect.cs
@@ -5,17 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour
 {
-    public void StartLevelX()
+    public static void StartLevel(string level, MonoBehaviour instance)
     {
-        StartCoroutine(loadLevelAsync("Level X"));
+        instance.StartCoroutine(loadLevelAsync(level));
     }
 
     public void Back()
     {
         StartCoroutine(loadLevelAsync(SceneManager.GetActiveScene().buildIndex - 1));
     }
+     public static void loadMainMenu(MonoBehaviour instance)
+    {
+       LevelSelect.StartLevel("Main Menu",instance);
+    }
+    public void loadMainMenu()
+    {
+        StartLevel("MainMenu", this);
+    }
 
-    IEnumerator loadLevelAsync(int index)
+    static IEnumerator loadLevelAsync(int index)
     {
         AsyncOperation asyncOp = SceneManager.LoadSceneAsync(index);
         //disable auto activation of new scene
@@ -38,7 +46,7 @@ public class LevelSelect : MonoBehaviour
         }
     }
 
-    IEnumerator loadLevelAsync(string sceneName)
+    static IEnumerator loadLevelAsync(string sceneName)
     {
         AsyncOperation asyncOp = SceneManager.LoadSceneAsync(sceneName);
         //disable auto activation of new scene
