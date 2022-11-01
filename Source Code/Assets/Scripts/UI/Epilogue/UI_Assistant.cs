c0f041b3794ba952b4cb7136db8253cb6e752bb7
Eli6a <sisa.elis@gmail.com>
2022-11-01 14:44:24 +0200

    typo

--- Source Code/Assets/Scripts/UI/Epilogue/UI_Assistant.cs
+++ Source Code/Assets/Scripts/UI/Epilogue/UI_Assistant.cs
@@ -16,7 +16,7 @@ public class UI_Assistant : MonoBehaviour
         messageText = transform.Find("Panel").Find("Message").GetComponent<Text>();
         epilogue = new List<string>();
         epilogue.Add("In a long distant world, a rabbit was born.Mimi was an unlucky kind; since birth, she was unable to differentiate colours. Not thinking much of it, she thought it was a completely normal thing.");
-        epilogue.Add("Years of criticism and confusion later, the surroundings made Mimi realise her one big lack; her sight of colors.It was not an easy thing to accept; Mimi simply refused to believe that others could see a wider range of colours than her.");
+        epilogue.Add("Years of criticism and confusion later, the surroundings made Mimi realise her one big lack; her sight of colors. It was not an easy thing to accept; Mimi simply refused to believe that others could see a wider range of colours than her.");
         epilogue.Add("Sorrowful months have passed by, until, one night, upon a deep look into the forest from her balcony, she saw something unseen before; it was a different, more fragrant shining shade of grey. It was almost as if... it was not grey?");
         epilogue.Add("Without a second thought, Mimi began her chase in fulfilling her inquiries about the witnessed anomaly.");
     }
